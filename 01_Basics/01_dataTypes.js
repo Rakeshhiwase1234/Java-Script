bb410de13989f21d5d2635f441d73e97c0e835c0
Rakesh Hiwase <hiwaserakesh1@gmail.com>
2025-08-13 04:17:10 +0000

    Disscuse dataTypes

---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert ( 3 + 3 )  // we are using nodejs ,not a browser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("rakesh");    
+
+let name = "rakesh";
+let age = 20;
+let isLoggedIn = false;
+
+
+    //  premitive data types 
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//booean => true / false
+//null => standalone value
+//undefine => 
+//symbol =>unique
+
+
+//object
+
+console.log(typeof undefined);  // undefine
+console.log(typeof null); // object
