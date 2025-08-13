0844c211bc02b97cb768bb8d81a965b02c260b44
Rakesh Hiwase <hiwaserakesh1@gmail.com>
2025-08-13 16:13:25 +0000

    datatypes-summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 Types = String, Boolean, Number,null, undefine, Symbol,BigInt
+
+const nam = "rakesh"
+
+const isStudent = true
+
+const score = 100
+
+const outsideTemp = null
+
+let userEmail ;
+
+// let id = symbol('1233')
+
+const bigNumber = 125468988n
+
+
+
+
+// Reference(non primitive)
+
+//3 types =Array, Object, Function
+
+let student = ["rakesh ", "prajwal", "Sarang"];
+
+let myInfo = {
+    name : "rakesh",
+    age :1,
+}
+
+const myFunction = function(){
+    console.log ("hello rakesh");
+}
+
+console.log(typeof nam );
+console.log(typeof isStudent);
+console.log(typeof score );
+console.log(typeof outsideTemp);
+console.log(typeof bigNumber);
