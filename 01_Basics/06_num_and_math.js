4db81e38d85001a85b29100493bf579a86016f4c
Rakesh Hiwase <hiwaserakesh1@gmail.com>
2025-08-17 13:02:27 +0000

    Discuss a num and maths

---
+++ 01_Basics/06_num_and_math.js
@@ -0,0 +1,51 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumber = 123.5452
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// ++++++++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(2.5));
+// console.log(Math.min(1,2,3,5));
+// console.log(Math.max(5,5,8,5));
+
+
+
+
+
+console.log(Math.random());
+
+console.log(Math.random()*10 + 1);
+console.log(Math.floor (random()*10) + 1);
+
+const min = 10
+const max = 20
+
+Math.random( Math.floor (Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
