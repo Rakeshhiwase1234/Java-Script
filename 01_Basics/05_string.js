fe2a4d0600aecb7c65441546cd17116f931aad6d
Rakesh Hiwase <hiwaserakesh1@gmail.com>
2025-08-17 12:29:19 +0000

    discuss a string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,30 @@
+const name = "Rakesh"
+const repoCount = 10
+
+// console.log(name + repoCount + "repo");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = "Cricket"
+
+console.log(gameName[0]);
+
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(`c`));
+
+
+const newString = gameName.substring(0,1)
+console.log(newString);
+
+const newStringOne = "  Rakesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
